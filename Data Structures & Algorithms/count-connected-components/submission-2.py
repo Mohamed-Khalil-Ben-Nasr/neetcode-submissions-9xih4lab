1a053c46cb86510b15d86810d1722a15949a8255
Mohamed Khalil Ben Nasr <mohamed_khalil_ben_nasr@brown.edu>
2026-06-03 17:19:29 +0100

    Add: count-connected-components - submission-2

---
+++ Data Structures & Algorithms/count-connected-components/submission-2.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        # create adjacency list
+        nodeToNeighbors = defaultdict(list)
+        for edge in edges:
+            a, b = edge[0], edge[1]
+            nodeToNeighbors[a].append(b)
+            nodeToNeighbors[b].append(a)
+        
+        visited = set()
+        def dfs(node):
+            if node in visited:
+                return
+            visited.add(node)
+            for nei in nodeToNeighbors[node]:
+                dfs(nei)
+            return
+        
+        res = 0
+        for node in range(n):
+            # dfs will be triggered when we start exploring the nodes of a new component
+            # and add all of the vertices of the new component to the visited set
+            if node not in visited:
+                dfs(node)
+                res += 1
+
+        return res
