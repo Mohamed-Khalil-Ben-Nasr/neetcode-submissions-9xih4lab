5103f64563504fc6aae38a5b49514315c9c9696d
Mohamed Khalil Ben Nasr <mohamed_khalil_ben_nasr@brown.edu>
2026-06-18 15:07:13 +0100

    Add: longest-palindromic-subsequence - submission-2

---
+++ Data Structures & Algorithms/longest-palindromic-subsequence/submission-2.py
@@ -0,0 +1,25 @@
+import sys
+sys.setrecursionlimit(10000)
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        # longest palindromic subsequence in s is basically
+        # LCS of s and reversed_s
+        r = s[::-1]
+        dp = {}
+        n = len(s)
+        def dfs(i,j):
+            # base cases
+            if (i,j) in dp:
+                return dp[(i,j)]
+            # if out-of-bounds
+            if (i < 0 or j < 0 or i >= n or j >= n):
+                return 0
+            # if chars match
+            if s[i] == r[j]:
+                dp[(i,j)] = 1 + dfs(i+1, j+1)
+            else:
+                dp[(i,j)] = max(dfs(i+1,j), dfs(i, j+1))
+            return dp[(i,j)]
+        return dfs(0,0)
+
+
