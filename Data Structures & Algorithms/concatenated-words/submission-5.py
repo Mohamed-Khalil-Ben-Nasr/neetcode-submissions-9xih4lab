a2e962c404da35d5e51a8b52118b05fc2e43b664
Mohamed Khalil Ben Nasr <mohamed_khalil_ben_nasr@brown.edu>
2026-06-19 16:37:46 +0100

    Add: concatenated-words - submission-5

---
+++ Data Structures & Algorithms/concatenated-words/submission-5.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
+        dp = {}
+        def dfs(s, i):
+            if (s,i) in dp:
+                return dp[(s,i)]
+            if i >= len(s):
+                return 0
+            dp[(s,i)] = float("-inf")
+            # explore all possible word matches to see which one leads to 
+            # the most concatenations
+            for word in words:
+                l = len(word)
+                if s[i:i+l] == word:
+                    cur = dfs(s, i+l)
+                    if cur != float("-inf"):
+                        dp[(s,i)] = max(dp[(s,i)],1 + cur)
+            return dp[(s,i)]
+        res = []
+        for word in words:
+            if dfs(word, 0) >= 2:
+                res.append(word)
+        return res
