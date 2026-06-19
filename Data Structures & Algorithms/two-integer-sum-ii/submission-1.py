ff3d0b9c693edf13ea80a5247da8ae6c265c2c1d
Mohamed Khalil Ben Nasr <mohamed_khalil_ben_nasr@brown.edu>
2026-06-19 22:15:31 +0100

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        left, right = 0, len(nums)-1
+        # since we cant use the same element twice, we cant consider
+        # the solution where the two pointers meet at the middle
+        while left < right:
+            total = nums[left] + nums[right]
+            if total == target:
+                return [left+1, right+1]
+            elif total > target:
+                right -= 1
+            else:
+                left += 1
+            
