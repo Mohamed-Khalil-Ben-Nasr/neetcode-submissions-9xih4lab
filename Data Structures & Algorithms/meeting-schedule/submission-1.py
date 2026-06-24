3c94b411979acac59a3c8945da7211c22abfc45a
Mohamed Khalil Ben Nasr <mohamed_khalil_ben_nasr@brown.edu>
2026-06-24 21:04:39 +0100

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.py
@@ -0,0 +1,22 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+    
+        sorted_intervals = []
+        for interval in intervals:
+            sorted_intervals.append([interval.start, interval.end])
+        
+        sorted_intervals.sort(key = lambda x: x[0])
+
+        for i in range(1,len(sorted_intervals)):
+            # if there is overlap => False
+            if sorted_intervals[i][0] < sorted_intervals[i-1][1]:
+                return False
+        return True
