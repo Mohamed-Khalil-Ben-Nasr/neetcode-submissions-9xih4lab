f1883c42266085db2d2ff21d2bd60ec07096c6a0
Mohamed Khalil Ben Nasr <mohamed_khalil_ben_nasr@brown.edu>
2026-07-06 13:35:41 +0100

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.py
@@ -0,0 +1,41 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        def dfs(o, combo, stack):
+            # success
+            if len(combo) == n * 2 and not stack:
+                res.append("".join(combo.copy()))
+                return
+            
+            # failure
+            if len(combo) == n*2:
+                return
+
+            # resetting state after visiting each branch in the decision
+            # tree is important, because this is not a simple include/exclude
+            # cuz in both branches we add an element
+
+            # 2 branches in the decision tree:
+            # add opening paranthesis
+            if o > 0:
+                combo.append("(")
+                stack.append("(")
+                dfs(o-1, combo, stack)
+                # reset only after visiting this branch
+                combo.pop()
+                stack.pop()
+            
+            # add closing paranthesis
+            # go down this branch only when it results in a valid combo
+            if stack and stack[-1] == "(":
+                stack.pop()
+                combo.append(")")
+                dfs(o, combo, stack)
+                # reset after visiting this branch
+                stack.append("(")
+                combo.pop()
+                
+            return
+        dfs(n-1, ["("], ["("])
+        return res
+
