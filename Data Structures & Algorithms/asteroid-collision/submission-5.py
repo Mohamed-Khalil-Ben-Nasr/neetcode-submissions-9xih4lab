34ba1a6595a12a010d20fa337bf294f773eef0e4
Mohamed Khalil Ben Nasr <mohamed_khalil_ben_nasr@brown.edu>
2026-08-06 22:46:33 +0100

    Add: asteroid-collision - submission-5

---
+++ Data Structures & Algorithms/asteroid-collision/submission-5.py
@@ -0,0 +1,17 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        for asteroid in asteroids:
+            # collision only happens if stack[-1] is moving right and the new asteroid is moving left
+            if (stack and stack[-1] > 0 and asteroid< 0):
+                while stack and stack[-1] > 0 and abs(asteroid) >   stack[-1]:
+                        stack.pop()
+                if stack and abs(asteroid) < stack[-1]:
+                    continue
+                elif stack and abs(asteroid) == stack[-1]:
+                    stack.pop()
+                else:
+                    stack.append(asteroid)
+            else:
+                stack.append(asteroid)
+        return stack
